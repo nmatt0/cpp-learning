771f0a147b79e21a3e5798734bc74869d7a81deb
Matt Brown <matt@nmatt.com>
2021-03-10 13:02:49 -0500

    fix downcast

--- oop/car.cpp
+++ oop/car.cpp
@@ -13,12 +13,12 @@ using namespace oop;
 		c1 = new sedan("Honda","Civic",2020);
 		c2 = new pickup("Ford","F150",2021,5000);
 		p3 = new pickup("Ford","F150",2021,5000);
-		p2 = (pickup*) &c2;
+		p2 = (pickup*)(c2); // explicit downcast
 
 		std::cout << c1->CarString() << std::endl << std::endl;
 
 		std::cout << c2->CarString() << std::endl;
-		std::cout << p2->HaulingLoad() << std::endl << std::endl; // Why does this print 0?? clearly some kind of issue casting pointer to pickup
+		std::cout << p2->HaulingLoad() << std::endl << std::endl;
 
 		std::cout << p3->CarString() << std::endl;
 		std::cout << p3->HaulingLoad() << std::endl << std::endl;
