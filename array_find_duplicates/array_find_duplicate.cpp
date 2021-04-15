40ffe4c62757c441cf45345f871cd1023dab3c3c
Matt Brown <matt@nmatt.com>
2021-04-15 13:45:26 -0400

    find duplicate in array in O(n)

---
+++ array_find_duplicates/array_find_duplicate.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+
+template <typename T>
+
+bool duplicate(std::vector<T> & v)
+{
+    std::unordered_set<T> s;
+    for (auto i : v)
+    {
+        auto item = s.find(i);
+        if (item != s.end())
+        {
+            return true;
+        }
+        s.insert(i);
+    }
+    return false;
+}
+
+int main()
+{
+    std::vector<int> v1 = {1,2,3,4,5,6,7,8,9};
+    std::vector<int> v2 = {1,2,3,4,5,6,1,8,9};
+
+    std::cout << "duplicate(v1): " << duplicate(v1) << std::endl;
+    std::cout << "duplicate(v2): " << duplicate(v2) << std::endl;
+    return 0;
+}
